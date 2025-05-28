193c4db7488ab90ba2d2a7080b6b7fb84f2e9c65
schlangoo <christoph.haroun@hotmail.de>
2025-05-28 11:35:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const RSS_FEEDS = {
     taz: "https://taz.de/!p4608;rss/",
     aljazeera: "https://www.aljazeera.com/xml/rss/all.xml",
     guardian: "https://www.theguardian.com/world/rss",
-    wire: "https://thewire.in/feed"
+    wire: "https://thewire.in/rss"
 };
 
 async function loadFeed(feedKey) {
