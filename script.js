2be1e2f23818eed1e3f3d55dc78953b73c070d98
schlangoo <christoph.haroun@hotmail.de>
2025-06-10 10:44:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const RSS_FEEDS = {
     jungewelt: "https://www.jungewelt.de/feeds/newsticker.rss",
     aljazeera: "https://www.aljazeera.com/xml/rss/all.xml",
     un: "https://news.un.org/feed/subscribe/en/news/all/rss.xml",
-    nachdenkenseiten: "https://www.nachdenkseiten.de/"
+    nachdenkenseiten: "https://multipolar-magazin.de/rss"
 };
 
 async function loadFeed(feedKey, initialLoad = false) {
