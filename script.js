e61c686557d51a9f65e8d6e0a819b7127816500b
schlangoo <christoph.haroun@hotmail.de>
2025-08-05 11:37:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const RSS_FEEDS = {
     deutschlandfunk: "https://www.deutschlandfunk.de/nachrichten-100.rss",
-    jungewelt: "https://www.blackagendareport.com/feeds-story",
+    jungewelt: "https://www.theroot.com/rss",
     aljazeera: "https://www.aljazeera.com/xml/rss/all.xml",
     bbc: "https://feeds.bbci.co.uk/news/rss.xml",
     lemonde: "https://www.lemonde.fr/en/rss/une.xml",
