30ac72af75fcc19bfda860283854d201a6122b40
schlangoo <christoph.haroun@hotmail.de>
2025-09-11 08:18:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const RSS_FEEDS = {
     deutschlandfunk: "https://www.deutschlandfunk.de/nachrichten-100.rss",
-    globaltimes: "https://english.cntv.cn/rss/index.shtml",
+    globaltimes: "http://www.cgtn.com/subscribe/rss/section/china.xml",
     aljazeera: "https://www.aljazeera.com/xml/rss/all.xml",
     bbc: "https://feeds.bbci.co.uk/news/rss.xml",
     lemonde: "https://www.lemonde.fr/en/rss/une.xml",
