fc8e1b41ed5dcfa70efd41373f4174696faeaa6a
schlangoo <christoph.haroun@hotmail.de>
2026-01-15 08:55:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const RSS_FEEDS = {
     deutschlandfunk: "https://www.deutschlandfunk.de/nachrichten-100.rss",
-    jungewelt: "https://www.untergrund-blättle.ch/rss/aktuelle_artikel.rss",
+    untergrundblättle: "https://www.untergrund-blättle.ch/rss/aktuelle_artikel.rss",
     aljazeera: "https://www.aljazeera.com/xml/rss/all.xml",
     bbc: "https://feeds.bbci.co.uk/news/rss.xml",
     lemonde: "https://www.lemonde.fr/en/rss/une.xml",
